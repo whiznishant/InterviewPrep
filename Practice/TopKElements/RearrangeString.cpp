0f6d45abcb0b86f38f923dbe792c70ec372b769f
Nishant <nishantsingh81@gmail.com>
2021-09-26 08:45:29 -0700

    Updated

--- Practice/TopKElements/RearrangeString.cpp
+++ Practice/TopKElements/RearrangeString.cpp
@@ -17,3 +17,59 @@ Input: "aapa"
 Output: ""
 Explanation: In all arrangements of "aapa", atleast two 'a' will come together e.g., "apaa", "paaa".
 */
+
+using namespace std;
+
+#include <iostream>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class RearrangeString {
+ public:
+  struct compare
+  {
+    bool operator()( pair<char,int> &lhs, pair<char,int> &rhs)
+    {
+      return lhs.second < rhs.second;
+    }
+  };
+  static string rearrangeString(const string &str) {
+    // TODO: Write your code here
+    unordered_map<char,int> freqMap;
+    for(auto x: str)
+      freqMap[x] += 1;
+    
+    priority_queue<pair<char,int>,vector<pair<char,int>>,compare> maxHeap;
+    unordered_map<char,int>::iterator it = freqMap.begin();
+    while(it != freqMap.end())
+    {
+      maxHeap.push(*it);
+      it++;
+    }
+
+    string out;
+    pair<char,int> prev('#',-1);
+    while(!maxHeap.empty())
+    {
+      auto top = maxHeap.top();
+      maxHeap.pop();
+      top.second -=1;
+      out += top.first;
+      if(prev.second > 0)
+        maxHeap.push(prev);      
+      prev = top;
+    }
+
+    if(out.length() == str.length())
+      return out;
+    return "";
+  } 
+};
+
+int main(int argc, char *argv[]) {
+  /*cout << "Rearranged string: " << RearrangeString::rearrangeString("aappp") << endl;
+  cout << "Rearranged string: " << RearrangeString::rearrangeString("Programming") << endl;*/
+  cout << "Rearranged string: " << RearrangeString::rearrangeString("aapa") << endl;
+}
